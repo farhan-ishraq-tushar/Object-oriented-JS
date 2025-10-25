b73aba6994c7e8d77914eb32ff27f6c0d2a427b7
unknown <farhanishraqtushar@gmail.com>
2025-10-25 08:42:01 +0600

    inheritance part complete

--- inheritance.js
+++ inheritance.js
@@ -24,3 +24,43 @@ console.log(SonObj); //Output: Son { num1: 10, num2: 20 }
 console.log(SonObj.num1); //Output: 10
 
 SonObj.addNumber(); //Output: 300  
+
+
+
+// ----------------------------------------------------------
+// Constructor behavior in inheritance
+
+
+
+
+
+
+
+
+
+// Case-3.2: Constructor inside both parent & child class parameters
+class myFather{
+     constructor(myFather_parameter){
+        console.log(myFather_parameter)
+     }
+}
+
+class mySon extends myFather{ 
+    constructor(mySon_parameter){
+        super();
+        console.log(mySon_parameter)
+    }
+}
+new myFather("Parameter from myFather"); /*Output: Parameter from myFather'.
+Only 'constructor' of 'myFather' executed.*/
+
+new mySon("Parameter from mySon"); 
+/*Output-1: undefined
+As, 'mySon' already has a 'parameter' it will not execute the 'parameter' 
+of myFather. But, still it will execute the constructor of 'myFather'. As,
+no valid parameter is passed the return type is 'undefined'. 
+
+Output-2: Parameter from mySon
+'Parameter' of 'mySon' executed.*/
+
+
